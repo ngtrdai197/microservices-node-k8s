d7a0a83ba6e2c22de042c0a81cf8435e43a3258b
Dai Nguyen <37446552+ngtrdai197@users.noreply.github.com>
2021-08-21 01:50:51 +0700

    Revert "chore(): Remove code unused"

--- client-app/pages/index.tsx
+++ client-app/pages/index.tsx
@@ -17,6 +17,11 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
         <div>
           <LinkWrap to={"/auth/signup"}>Sign up</LinkWrap>
         </div>
