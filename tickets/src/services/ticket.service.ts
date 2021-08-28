71ee1a31d889b69559b3338a4eed17be36959405
Dai Nguyen <ngtrdai290197@gmail.com>
2021-08-28 16:39:58 +0700

    feat(ticket): Add CRUD

--- tickets/src/services/ticket.service.ts
+++ tickets/src/services/ticket.service.ts
@@ -1,4 +1,7 @@
+import { NotFoundError, parseObjectID } from "@dnt-ticketing-mvc/shared";
 import { Request, Response } from "express";
+import { IRequest } from "../interfaces/common.interface";
+import { ticketModel } from "../models/ticket.model";
 
 export class TicketService {
   private static instance: TicketService;
@@ -11,30 +14,41 @@ export class TicketService {
     return TicketService.instance;
   }
 
-  public async createTicket(req: Request, resp: Response) {
-    console.log("req.body :>> ", req.body);
-    return resp
-      .status(201)
-      .jsonp({ statusCode: 201, msg: "Create new ticket successfully" });
+  public async createTicket(req: IRequest, resp: Response) {
+    const ticket = await ticketModel
+      .build({
+        userId: req.user!.id,
+        title: "Summer Party",
+        price: 50,
+      })
+      .save();
+    return resp.status(201).jsonp({ statusCode: 201, data: ticket });
   }
 
   public async editTicket(req: Request, resp: Response) {
-    console.log("req.body :>> ", req.body);
-    return resp
-      .status(200)
-      .jsonp({ statusCode: 200, msg: "Edit ticket successfully" });
+    const ticketId = parseObjectID(req.params.ticketId);
+
+    const ticket = await ticketModel.findByIdAndUpdate(ticketId, req.body);
+    if (!ticket) {
+      throw new NotFoundError(`Cannot found ticket with ID: ${ticketId}`);
+    }
+    return resp.status(200).jsonp({
+      statusCode: 200,
+      data: ticket,
+    });
   }
 
-  public async getTickets(req: Request, resp: Response) {
-    return resp
-      .status(200)
-      .jsonp({ statusCode: 200, msg: "Get tickets successfully" });
+  public async getTickets(_: Request, resp: Response) {
+    const tickets = await ticketModel.find({});
+    return resp.status(200).jsonp({ statusCode: 200, data: tickets });
   }
 
   public async getTicketById(req: Request, resp: Response) {
-    console.log("req.params.ticketId :>> ", req.params.ticketId);
-    return resp
-      .status(200)
-      .jsonp({ statusCode: 200, msg: "Get ticket by ID successfully" });
+    const ticketId = parseObjectID(req.params.ticketId);
+    const ticket = await ticketModel.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError(`Cannot found ticket with ID: ${ticketId}`);
+    }
+    return resp.status(200).jsonp({ statusCode: 200, data: ticket });
   }
 }
